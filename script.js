f1f47b5719ca46252ff132200c25f724f8cf97a1
pominovaa <alexandra.pominova.2010@gmail.com>
2020-12-25 17:15:48 -0500

    progress bar movement on click

--- script.js
+++ script.js
@@ -123,6 +123,15 @@ function updateProgressBar(e) {
     }
 }
 
+// set progress
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX
+    const { duration } = music;
+    music.currentTime = (clickX / width) * duration;
+
+}
+
 // on load - select first song
 loadSong(songs[songIndex]);
 
@@ -130,7 +139,10 @@ loadSong(songs[songIndex]);
 // event liasteners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong)
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
+
 
 
 
